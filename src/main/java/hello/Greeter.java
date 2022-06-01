4565ce80234cb2a9ac899d08e9ef317a67125b33
NebihaAmir <craftsoftwarenebiha@gmail.com>
2022-06-01 16:44:23 +0300

    file updated

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world- This is Nebiha simple mvn project ...    ... .. . . . . . ....... ... 🤨";
+    return "Hello world- This is Nebiha simple mvn project ...    ... .. . . . . . ....... ... 🤨 Bye";
   }
 }
